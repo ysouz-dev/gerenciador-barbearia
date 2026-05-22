4e10aa8f932d56c0ca40b69eb64c351d564283b5
Yago Souza <yagosouza.1802@gmail.com>
2026-05-22 16:12:44 -0300

    feat: adiciona fluxo principal ao main

--- src/main/java/com/ysouz/gerenciadorbarbearia/Main.java
+++ src/main/java/com/ysouz/gerenciadorbarbearia/Main.java
@@ -1,4 +1,47 @@
 package com.ysouz.gerenciadorbarbearia;
 
+import com.ysouz.gerenciadorbarbearia.controller.Menu;
+
 public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        int escolha = Integer.MIN_VALUE;
+        while (escolha != 0) {
+            escolha = menu.MenuPrincipal();
+
+            switch (escolha) {
+                case 1:
+                    menu.cadastrarCliente();
+                    break;
+
+                case 2:
+                    menu.cadastrarAtendimento();
+                    break;
+
+                case 3:
+                    menu.listarClientes();
+                    break;
+
+                case 4:
+                    menu.listarAtendimentos();
+                    break;
+
+                case 5:
+                    menu.removerCliente();
+                    break;
+
+                case 6:
+                    menu.removerAtendimento();
+                    break;
+
+                case 7:
+                    menu.estatisticas();
+                    break;
+
+                case 0:
+                    menu.encerrarSistema();
+                    break;
+            }
+        }
+    }
 }
