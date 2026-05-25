087351a09ea78073d23047dfb261f514e4cb3e0f
Yago Souza <yagosouza.1802@gmail.com>
2026-05-25 15:07:29 -0300

    refactor: altera estrutura do repositorio de cliente para Map

--- src/main/java/com/ysouz/gerenciadorbarbearia/repository/ClienteDiarioRepository.java
+++ src/main/java/com/ysouz/gerenciadorbarbearia/repository/ClienteDiarioRepository.java
@@ -3,31 +3,30 @@ package com.ysouz.gerenciadorbarbearia.repository;
 import com.ysouz.gerenciadorbarbearia.model.Pessoa;
 import com.ysouz.gerenciadorbarbearia.model.ClienteDiario;
 
+import java.util.Map;
 import java.util.ArrayList;
 
 public class ClienteDiarioRepository {
-    private ArrayList<Pessoa> listaPessoas;
+    private Map<String, Pessoa> listaPessoas;
 
     public void salvar(ClienteDiario cliente) {
-        this.listaPessoas.add(cliente);
+        this.listaPessoas.put(cliente.getCPF(), cliente);
     }
 
     public void remover(String cpf) {
         Pessoa c = buscaPorCpf(cpf);
-        this.listaPessoas.remove(c);
+        this.listaPessoas.remove(cpf, c);
     }
 
     public Pessoa buscaPorCpf(String cpf) {
-        for (Pessoa pessoa : this.listaPessoas) {
-            if (pessoa.getCPF().equals(cpf)) {
-                return pessoa;
-            }
+        if (this.listaPessoas.containsKey(cpf)) {
+            return this.listaPessoas.get(cpf);
         }
         throw new IllegalArgumentException("Nenhum cliente encontrado com esse cpf.");
     }
 
     public ArrayList<Pessoa> listarPessoas() {
-        return new ArrayList<Pessoa>(this.listaPessoas);
+        return new ArrayList<Pessoa>(this.listaPessoas.values());
     }
 
 
