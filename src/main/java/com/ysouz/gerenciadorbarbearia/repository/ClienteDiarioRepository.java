885bd4e1f2a90b1627d133970e6835f3c94654d0
Yago Souza <yagosouza.1802@gmail.com>
2026-05-25 14:24:20 -0300

    feat: adiciona pacote repository e classe ClienteDiarioRepository

---
+++ src/main/java/com/ysouz/gerenciadorbarbearia/repository/ClienteDiarioRepository.java
@@ -0,0 +1,11 @@
+package com.ysouz.gerenciadorbarbearia.repository;
+
+import com.ysouz.gerenciadorbarbearia.model.Pessoa;
+import com.ysouz.gerenciadorbarbearia.model.ClienteDiario;
+
+import java.util.ArrayList;
+
+public class ClienteDiarioRepository {
+    private ArrayList<Pessoa> listaPessoas;
+
+}
