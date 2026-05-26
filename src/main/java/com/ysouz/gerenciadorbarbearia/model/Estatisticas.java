6ec44783eb2e7b6fd7f5a6000f047e52d87c48e6
Yago Souza <yagosouza.1802@gmail.com>
2026-05-26 02:43:32 -0300

    feat: adiciona entidade Estatisticas

---
+++ src/main/java/com/ysouz/gerenciadorbarbearia/model/Estatisticas.java
@@ -0,0 +1,15 @@
+package com.ysouz.gerenciadorbarbearia.model;
+
+import java.math.BigDecimal;
+
+public class Estatisticas {
+    private int totalClientes;
+    private int totalAtendimentos;
+    private BigDecimal totalFaturado;
+
+    public Estatisticas() {
+        this.totalClientes = 0;
+        this.totalAtendimentos = 0;
+        this.totalFaturado = BigDecimal.ZERO;
+    }
+}
