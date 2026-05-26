a1273d4e26bf151c3ed8be7e744dbce91a60830d
Yago Souza <yagosouza.1802@gmail.com>
2026-05-26 01:42:01 -0300

    feat: adiciona AtendimentoRepository

---
+++ src/main/java/com/ysouz/gerenciadorbarbearia/repository/AtendimentoRepository.java
@@ -0,0 +1,10 @@
+package com.ysouz.gerenciadorbarbearia.repository;
+
+import com.ysouz.gerenciadorbarbearia.model.Atendimento;
+
+import java.util.Map;
+
+public class AtendimentoRepository {
+    private Map<Integer, Atendimento> listaAtendimento;
+
+}
