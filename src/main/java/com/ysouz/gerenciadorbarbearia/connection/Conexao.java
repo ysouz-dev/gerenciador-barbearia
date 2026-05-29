67e9bf39c0fafa37f84b8a4444d89f324b4acabe
Yago Souza <yagosouza.1802@gmail.com>
2026-05-29 15:35:43 -0300

    feat: adiciona bloco estatico que carrega as configuracoes do banco

--- src/main/java/com/ysouz/gerenciadorbarbearia/connection/Conexao.java
+++ src/main/java/com/ysouz/gerenciadorbarbearia/connection/Conexao.java
@@ -1,10 +1,18 @@
 package com.ysouz.gerenciadorbarbearia.connection;
 
 import java.util.Properties;
+import java.io.IOException;
 
 public abstract class Conexao {
     private static final Properties prop = new Properties();
 
+    static {
+        try {
+            prop.load(Conexao.class.getClassLoader().getResourceAsStream("dataabase.properties"));
 
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar configurações do banco de dados: " + e.getMessage());
+        }
+    }
 
 }
