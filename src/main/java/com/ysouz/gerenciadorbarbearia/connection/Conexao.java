7d172ef5734488191510da67ef13262d380a598d
Yago Souza <yagosouza.1802@gmail.com>
2026-05-29 15:18:19 -0300

    feat: adiciona atributo estatico a classe Conexao

--- src/main/java/com/ysouz/gerenciadorbarbearia/connection/Conexao.java
+++ src/main/java/com/ysouz/gerenciadorbarbearia/connection/Conexao.java
@@ -1,6 +1,10 @@
 package com.ysouz.gerenciadorbarbearia.connection;
 
+import java.util.Properties;
+
 public abstract class Conexao {
+    private static final Properties prop = new Properties();
+
 
 
 }
