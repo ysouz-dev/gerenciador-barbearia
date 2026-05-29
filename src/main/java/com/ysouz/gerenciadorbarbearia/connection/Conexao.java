b84b97f3f75b43ce4116d247e3d3603fbbcfef20
Yago Souza <yagosouza.1802@gmail.com>
2026-05-29 15:40:11 -0300

    feat: adiciona metodo que retorna conexao com o banco

--- src/main/java/com/ysouz/gerenciadorbarbearia/connection/Conexao.java
+++ src/main/java/com/ysouz/gerenciadorbarbearia/connection/Conexao.java
@@ -2,6 +2,10 @@ package com.ysouz.gerenciadorbarbearia.connection;
 
 import java.util.Properties;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 
 public abstract class Conexao {
     private static final Properties prop = new Properties();
@@ -12,6 +16,18 @@ public abstract class Conexao {
 
         } catch (IOException e) {
             System.out.println("Erro ao carregar configurações do banco de dados: " + e.getMessage());
+        }
+    }
+
+    public static Connection getConexao() {
+        try {
+            String url = prop.getProperty("database.url");
+            String user = prop.getProperty("database.user");
+            String pass = prop.getProperty("database.pass");
+
+            return DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar com o banco de dados: " + e.getMessage());
         }
     }
 
