153959b561550dc781f489684813f6db13d18c99
Yago Souza <yagosouza.1802@gmail.com>
2026-06-07 00:10:26 -0300

    refactor: implementa remocao dos dados da tabela de atendimentos_servicos do banco

--- src/main/java/com/ysouz/gerenciadorbarbearia/repository/AtendimentoRepository.java
+++ src/main/java/com/ysouz/gerenciadorbarbearia/repository/AtendimentoRepository.java
@@ -47,8 +47,13 @@ public class AtendimentoRepository {
 
     public void remover(Integer id) {
         String query = "DELETE FROM atendimentos WHERE id = ?";
+        String query2 = "DELETE FROM atendimentos_servicos WHERE id_atendimento = ?";
         try (Connection conexao = Conexao.getConexao();
-            PreparedStatement statement = conexao.prepareStatement(query)) {
+            PreparedStatement statement = conexao.prepareStatement(query);
+            PreparedStatement statement2 = conexao.prepareStatement(query2)) {
+            statement2.setInt(1, id);
+            statement2.executeUpdate();
+
             statement.setInt(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
