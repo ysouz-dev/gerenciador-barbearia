b5fb0198f3f85f54c178cab9977c9886a360518d
Yago Souza <yagosouza.1802@gmail.com>
2026-06-08 21:26:59 -0300

    refactor: remove metodo aumentarAtendimento

--- src/main/java/com/ysouz/gerenciadorbarbearia/model/ClienteDiario.java
+++ src/main/java/com/ysouz/gerenciadorbarbearia/model/ClienteDiario.java
@@ -14,10 +14,6 @@ public class ClienteDiario extends Pessoa {
         this.totalAtendimentos = totalAtendimentos;
     }
 
-    public void aumentarAtendimento() {
-        this.totalAtendimentos++;
-    }
-
     @Override
     public String resumo() {
         return super.resumo() + "\nTotal de atendimentos: " + this.totalAtendimentos;
