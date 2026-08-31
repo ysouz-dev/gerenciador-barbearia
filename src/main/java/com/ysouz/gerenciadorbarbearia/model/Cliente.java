ea4f84e19dc9aeb65c9511597cddf8e79e62e3b2
Yago Souza <yagosouza.1802@gmail.com>
2026-08-31 01:57:20 -0300

    refactor: remove metodo resumo, construtor e atributo totalAtendimentos sem uso

--- src/main/java/com/ysouz/gerenciadorbarbearia/model/Cliente.java
+++ src/main/java/com/ysouz/gerenciadorbarbearia/model/Cliente.java
@@ -5,7 +5,6 @@ import com.ysouz.gerenciadorbarbearia.enums.Sexo;
  * Representa um cliente diário no sistema.
  */
 public class Cliente extends Pessoa {
-    private final int totalAtendimentos;
 
     /**
      * Cria um cliente diário validando os dados informados.
@@ -19,36 +18,5 @@ public class Cliente extends Pessoa {
      */
     public Cliente(String nome, int idade, String cpf, Sexo sexo) {
         super(nome, idade, cpf, sexo);
-        this.totalAtendimentos = 0;
-    }
-
-    /**
-     * Cria um cliente diário validando os dados informados.
-     * <p>
-     * Recebe o total de atendimentos como parâmetro no construtor.
-     *
-     * @param nome nome do cliente
-     * @param idade idade do cliente
-     * @param cpf cpf do cliente
-     * @param sexo sexo do cliente
-     * @param totalAtendimentos total de atendimentos do cliente
-     * @throws IllegalArgumentException se algum dos dados informados forem inválidos ou nulos
-     * (nome, idade, cpf, sexo)
-     */
-    public Cliente(String nome, int idade, String cpf, Sexo sexo, int totalAtendimentos) {
-        super(nome, idade, cpf, sexo);
-        this.totalAtendimentos = totalAtendimentos;
-    }
-
-    /**
-     * Retorna uma ‘string’ com um resumo do cliente.
-     * <p>
-     * Também retorna o total de atendimentos do cliente.
-     *
-     * @return uma ‘string’ com os dados do cliente
-     */
-    @Override
-    public String resumo() {
-        return super.resumo() + "\nTotal de atendimentos: " + this.totalAtendimentos;
     }
 }
